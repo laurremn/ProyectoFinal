f66ff5b1ed8d5260611911bd75cb2633e51c34e0
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-07-12 17:21:09 -0500

    desloggeo

--- src/controllers/controlador.js
+++ src/controllers/controlador.js
@@ -1,12 +1,14 @@
 
-
-function cambiarCaja(){
-    let boton=document.getElementById("botonChat")
+let boton=document.getElementById("botonChat")
+boton.addEventListener("click",function(){
     let cajaChat=document.getElementById("cajaChat")
-
-    if (cajaChat.style.display == "none" || cajaChat.style.display==""){
+    if(cajaChat.style.display=="none" || cajaChat.style.display==""){
         cajaChat.style.display="block"
     }else{
         cajaChat.style.display="none"
     }
-}
+})
+
+
+
+
--- src/controllers/controladorlogin.js
+++ src/controllers/controladorlogin.js
@@ -1,8 +1,10 @@
-
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-analytics.js";
-  import { getAuth, signInWithPopup, GoogleAuthProvider } from 'https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js'
+  
+  import { getAuth,signInWithPopup,GoogleAuthProvider } from 'https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js'
+
+
 
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
@@ -10,13 +12,13 @@
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
-    apiKey: "AIzaSyBsQA1rcSC_BHgxkerVYAfX6LGFYqiQMQE",
-    authDomain: "loginsuraduallrm.firebaseapp.com",
-    projectId: "loginsuraduallrm",
-    storageBucket: "loginsuraduallrm.appspot.com",
-    messagingSenderId: "1029572821104",
-    appId: "1:1029572821104:web:1724d74fb53f65f65ff4b5",
-    measurementId: "G-YE6GCS1CZR"
+    apiKey: "AIzaSyATaFS3y2HtZA4y-3WQm7fRtbsaAycB6uU",
+    authDomain: "loginsuradualjjg.firebaseapp.com",
+    projectId: "loginsuradualjjg",
+    storageBucket: "loginsuradualjjg.appspot.com",
+    messagingSenderId: "270841800332",
+    appId: "1:270841800332:web:99ae746100c68433effda9",
+    measurementId: "G-3NGP4DKQEW"
   };
 
   // Initialize Firebase
@@ -26,11 +28,13 @@
 
   //rutina para detectar clic en el boton de google
   let botonLogin=document.getElementById("ingresoGmail")
+
   botonLogin.addEventListener("click",function(){
     const auth = getAuth();
-    const provider=new GoogleAuthProvider()
+    const provider= new GoogleAuthProvider()
+
     signInWithPopup(auth, provider)
-  .then((result) => {
+    .then((result) => {
     // This gives you a Google Access Token. You can use it to access the Google API.
     const credential = GoogleAuthProvider.credentialFromResult(result);
     const token = credential.accessToken;
@@ -51,4 +55,3 @@
     console.log(errorMessage)
   });
   })
- 
